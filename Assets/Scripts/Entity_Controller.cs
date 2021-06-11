30c08deeccaa81adeba5609303b9b06a00a651d0
Pontus Johansson <PontusJohansson@live.com>
2021-06-12 00:40:40 +0200

    Main char blobert

--- Assets/Scripts/Entity_Controller.cs
+++ Assets/Scripts/Entity_Controller.cs
@@ -6,10 +6,12 @@ public class Entity_Controller : MonoBehaviour
 {
     protected Vector2 move;
     protected Rigidbody2D body;
+    protected new SpriteRenderer renderer;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        renderer = GetComponentInChildren<SpriteRenderer>(true);
         AwakeInit();
     }
     private void Start()
